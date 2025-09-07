b2c12f2d6c4f271648f875cf493742b2daca3462
Ne-n-e <vindook@yahoo.com>
2025-09-07 07:05:10 -0400

    Fix error of using super coin states outside the component function

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,23 +1,24 @@
 import { useState, useEffect } from "react";
-
-
-const [superCoins, setSuperCoins] = useState(0);
-const cartItems = useSelector(state => state.cart.cartItems);
-const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-
-useEffect(() => {
-  if (totalAmount >= 100 && totalAmount < 200) {
-    setSuperCoins(10);
-  } else if (totalAmount >= 200 && totalAmount < 300) {
-    setSuperCoins(20);
-  } else if (totalAmount >= 300) {
-    setSuperCoins(30);
-  } else {
-    setSuperCoins(0);
-  }
-}, [totalAmount]);
+import { useSelector } from "react-redux";
 
 const SuperCoin = () => {
+
+    const [superCoins, setSuperCoins] = useState(0);
+    const cartItems = useSelector(state => state.cart.cartItems);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    useEffect(() => {
+        if (totalAmount >= 100 && totalAmount < 200) {
+          setSuperCoins(10);
+        } else if (totalAmount >= 200 && totalAmount < 300) {
+          setSuperCoins(20);
+        } else if (totalAmount >= 300) {
+          setSuperCoins(30);
+        } else {
+          setSuperCoins(0);
+        }
+      }, [totalAmount]);
+      
     return(
         <>
             <div className="super-coins" style={{textAlign:'center'}}>
